17ce24aa4128d2472bee55c3ee230de9f13482b3
小原　永暉 <obara@oharahisashiakiranoMacBook-Pro.local>
2021-06-03 04:13:27 +0900

    コード1-1

---
+++ java1_1.java
@@ -0,0 +1,8 @@
+public class java1_1 {
+  public static void main (String[] args) {
+    System.out.println("RPG:スッキリ魔王討伐") ;
+    System.out.println("Ver.0.1 by 奏") ;
+    System.out.println("＜ただいま学習・製作中") ;
+    System.out.println("プログラムを終了します。") ;
+  }
+}
