3152a7b03460312c817f8fd990f2182804796276
obara134 <tyobara@icloud.com>
2021-06-10 20:22:19 +0900

    4章終了

---
+++ java4_14.java
@@ -0,0 +1,8 @@
+public class java4_14{
+  public static void main (String[] args){
+    int[] scores = {20, 30, 40, 50, 80};
+    for (int value : scores){
+      System.out.println(value);
+    }
+  }
+}
---
+++ java4_p.java
@@ -0,0 +1,28 @@
+public class java4_p{
+  public static void main (String[] args){
+  // // 練習問題４−１
+  //   int[] points = new int[4];
+  //   double[] weights = new double[5];
+  //   boolean[] answers = new boolean[3];
+  //   String[] names =  new String[3];
+
+  // // 練習問題４−２
+  //   int[] moneyList = {121902, 8302, 55100};
+  //   for (int i = 0; i < moneyList.length; i++){
+  //     System.out.println(moneyList[i]);
+  //   }
+  //   for (int value : moneyList){
+  //     System.out.println(value);
+  //   }
+
+// 練習問題４−４
+//     int[] numbers = {3, 4, 9};
+//     System.out.println("１桁の数字を入力してください");
+//     int input = new java.util.Scanner(System.in).nextInt();
+//     for (int n : numbers){
+//       if (input == n){
+//         System.out.println("あたり");
+//       }
+//     }
+  }
+}
